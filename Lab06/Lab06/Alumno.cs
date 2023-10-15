06e422dac4dca52568c796bca26a8aa66b8eba23
Your Name <you@example.com>
2023-10-14 23:58:55 -0500

    Agregar archivos de proyecto.

---
+++ Lab06/Lab06/Alumno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06
+{
+    public class Alumno
+    {
+        public string Name { get; set; }
+        public bool IsExpanded { get; set; }
+    }
+}
---
+++ Lab06/Lab06/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab06
+{
+    public partial class MainPage : ContentPage
+    {
+        private List<Alumno> nameItems;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            nameItems = new List<Alumno>
+            {
+                new Alumno { Name = "Eduardo", IsExpanded = false },
+                new Alumno { Name = "Bryan", IsExpanded = false },
+                new Alumno { Name = "Johan", IsExpanded = false }
+            };
+
+            foreach (var nameItem in nameItems)
+            {
+                var viewCell = new ViewCell
+                {
+                    View = new StackLayout
+                    {
+                        Orientation = StackOrientation.Vertical,
+                        Children =
+                        {
+                            new Label { Text = nameItem.Name },
+                            new ContentView
+                            {
+                                Content = new StackLayout
+                                {
+                                    Children =
+                                    {
+                                        new Label { Text = "Detalles 1" },
+                                        new Label { Text = "Detalles 2" }
+                                    },
+                                    IsVisible = nameItem.IsExpanded
+                                }
+                            }
+                        }
+                    }
+                };
+
+                viewCell.Tapped += (sender, e) =>
+                {
+                    nameItem.IsExpanded = !nameItem.IsExpanded;
+                    ((StackLayout)((ContentView)viewCell.View).Content).IsVisible = nameItem.IsExpanded;
+                };
+
+                nameTableView.Root[0].Add(viewCell);
+            }
+        }
+    }
+}
